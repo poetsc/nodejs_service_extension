71f3340b12d05abca563d6abc76e9cc7a85a6f55
lsc <lsc7@foxmail.com>
2020-04-26 00:24:19 +0800

    1.增加错误处理 2.增加零停机重启

--- copy_instance/app.js
+++ copy_instance/app.js
@@ -2,8 +2,12 @@ const http = require('http');
 const pid = process.pid;
 http.createServer((req, res) => {
     for (let i = 1e7; i > 0; i--) { }
-    console.log(`Handing request from ${pid}`);
+    // console.log(`Handing request from ${pid}`);
     res.end(`Hello from ${pid} \n`);
 }).listen(8080, ()=> {
     console.log(`Started ${pid}`);
 })
+
+// setTimeout(() => {
+//     throw new Error('Ooops');
+// }, Math.ceil(Math.random() * 3) * 1000);
--- copy_instance/clusteredApp.js
+++ copy_instance/clusteredApp.js
@@ -1,12 +1,42 @@
 const cluster = require('cluster');
 const os = require('os');
 
-if(cluster.isMaster){
+if (cluster.isMaster) {
     const cpus = os.cpus().length;
     console.log(`Clustering to ${cpus} CPUs`);
-    for(let i =0; i < cpus; i ++){
+    for (let i = 0; i < cpus; i++) {
         cluster.fork();
     }
-}else{
+
+    //handle error
+    cluster.on('exit', (Worker, code) => {
+        if (code != 0 && !Worker.suicide) {
+            console.log('Worker crashed. Starting a new worker');
+            cluster.fork();
+        }
+    })
+
+    //零停机重启
+    process.on('SIGUSR2', () => {
+        const workers = Object.keys(cluster.workers);
+        function restartWorker(i) {
+            if (i >= workers.length) return;
+            const worker = cluster.workers[workers[i]];
+            console.log(`Stopping worker: ${worker.process.pid}`);
+            worker.disconnect();
+            worker.on('exit', () => {
+                /* In Node.js 6.0.0, the worker.suicide was deprecated and replaced with a new worker.exitedAfterDisconnect property. */ 
+                // if (!worker.suicide) return;
+                if (!worker.exitedAfterDisconnect) return;
+                const newWorker = cluster.fork();
+                newWorker.on('listening', () => {
+                    restartWorker(i + 1);
+                })
+            })
+        }
+        restartWorker(0);
+    });
+
+} else {
     require('./app')
 }
